0d35904fadc3655a3d6bd36f9be32b6ed7646f1b
GamerVincent <andy_bartolo@yahoo.com>
2013-07-03 08:10:21 -0700

    Create GamerUtil.java

---
+++ GamerUtil.java
@@ -0,0 +1,22 @@
+package com.GamerPlugins.core.Util;
+
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.FileConfigurationOptions;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GamerUtil
+{
+  public final Logger logger = Logger.getLogger("Minecraft");
+        if GamerPlugin=!null;
+                this.JavaPlugin = GamerPlugin;
+
+	if GamerPlugin.equalsIgnoreCase("GamerCore") {
+		this.logger.info("[GamerUtil] GamerCore found");
+		  }
+	}
